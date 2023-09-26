86e98d5fcc7764f2e361c43f31861f7465a323e1
Joseph Long <github@joseph-long.com>
2023-09-26 19:12:16 -0400

    Provisioning script cleanup

--- scripts/rtimv_provision_ubuntu2204.sh
+++ scripts/rtimv_provision_ubuntu2204.sh
@@ -20,7 +20,9 @@ mkdir -p ~/Source
 cd ~/Source
 
 ## xrif
-git clone https://github.com/jaredmales/xrif.git
+if [[ ! -d xrif ]]; then
+    git clone https://github.com/jaredmales/xrif.git
+fi
 mkdir -p ./xrif/_build
 cd ./xrif/_build
 cmake ..
@@ -29,21 +31,27 @@ sudo make install
 
 ## zeromq drafts API
 cd ~/Source
-wget https://github.com/zeromq/libzmq/releases/download/v4.3.4/zeromq-4.3.4.tar.gz
-tar -xvzf zeromq-4.3.4.tar.gz
+if [[ ! -d zeromq-4.3.4 ]]; then
+    wget https://github.com/zeromq/libzmq/releases/download/v4.3.4/zeromq-4.3.4.tar.gz
+    tar -xvzf zeromq-4.3.4.tar.gz
+fi
 cd zeromq-4.3.4
 ./configure --enable-drafts
 make
 sudo make install
 
 cd ~/Source
-git clone https://github.com/zeromq/cppzmq.git
+if [[ ! -d cppzmq ]]; then
+    git clone https://github.com/zeromq/cppzmq.git
+fi
 cd cppzmq/
 sudo cp *.hpp /usr/local/include/
 
 ## milkzmq
 cd ~/Source
-git clone https://github.com/jaredmales/milkzmq.git
+if [[ ! -d milkzmq ]]; then
+    git clone https://github.com/jaredmales/milkzmq.git
+fi
 cd milkzmq
 git checkout dev
 make
@@ -51,11 +59,13 @@ sudo make install
 
 ## mxlib
 cd ~/Source
-sudo apt install -y libgsl-dev libboost-all-dev libcfitsio-dev libopenblas-dev libfftw3-dev libeigen3-dev
-git clone https://github.com/jaredmales/mxlib.git
+sudo apt-get install -y libgsl-dev libboost-all-dev libcfitsio-dev libopenblas-dev libfftw3-dev libeigen3-dev
+if [[ ! -d mxlib ]]; then
+    git clone https://github.com/jaredmales/mxlib.git
+fi
 cd mxlib
 git checkout dev
-echo NEED_CUDA=no >> local/Common.mk
+echo NEED_CUDA=no > local/Common.mk
 echo PREFIX=/usr/local >> local/Common.mk
 make
 sudo make install
@@ -63,7 +73,9 @@ sudo make install
 ## rtimv
 cd ~/Source
 sudo apt install -y qtbase5-dev qt5-qmake
-git clone https://github.com/jaredmales/rtimv.git
+if [[ ! -d rtimv ]]; then
+    git clone https://github.com/jaredmales/rtimv.git
+fi
 cd rtimv
 git checkout dev
 make
